65d7f1f87aeac5f284a272def287673286f874fc
zsu <azure.ss@gmail.com>
2014-08-29 16:09:57 -0400

    Add missing files

---
+++ src/Web/app/logs/logsApiService.js
@@ -0,0 +1,11 @@
+﻿(function () {
+    /**
+     * @param $http
+     * @param $q
+     * @param $angularCacheFactory
+     */
+    $app.LogsApi = function ($http, $q, $angularCacheFactory) {
+    };
+
+    app.service("logsApi", ["$http", "$q", "$angularCacheFactory", $app.LogsApi]);
+})();
